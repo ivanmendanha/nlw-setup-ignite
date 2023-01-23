8e2fb4f721dbb9b49f83d745247250fdabf11312
Lumen Soluções <ivanmendanha@gmail.com>
2023-01-22 22:45:39 -0300

    Aula 05

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -54,7 +54,7 @@ export function NewHabitForm() {
         type="text"
         id="title"
         placeholder="ex.: Exercícios, dormir bem, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outine-none focus:ring-2 focus:ring-violet-700 focus: ring-offset-2 focus:ring-offset-zinc-900"
         autoFocus
         value={title}
         onChange={event => setTitle(event.target.value)}
@@ -68,7 +68,7 @@ export function NewHabitForm() {
         {availableWeekDays.map((weekDay, index) => (
           <Checkbox.Root
             key={weekDay}
-            className='flex items-center gap-3 group pl-1 focus:outline-none hover:border-none focus:border-none'
+            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed py-2 px-0 border-none hover:border-none'
             checked={weekDays.includes(index)}
             onCheckedChange={() => handleToggleWeekDay(index)}
           >
@@ -86,7 +86,10 @@ export function NewHabitForm() {
       </div>
 
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button 
+        type="submit" 
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+      >
         <Check size={20} weight="bold" /> Confirmar
       </button>
     </form>
